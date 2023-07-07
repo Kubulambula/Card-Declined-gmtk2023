020e75eefc50d249afd33a88cc69ad2fca296cca
Jakub Janšta <jansta.ja@gmail.com>
2023-07-07 21:08:01 +0200

    initial stuff

---
+++ scenes/car.gd
@@ -0,0 +1,18 @@
+extends RigidBody2D
+
+
+var acceleration: float = 1
+var turn_factor: float = 1
+
+var gas_input: float = 0
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta: float) -> void:
+	
+	pass
